532cd87c06b336da5c90043b1b4c493e594600b0
kts of kettek (nyaa) <kettek1@kettek.net>
2020-06-17 21:01:44 -0700

    Properly Register types

--- network/Register.go
+++ network/Register.go
@@ -10,4 +10,12 @@ func RegisterCommands() {
 	gob.RegisterName("M", CommandMap{})
 	gob.RegisterName("L", CommandLogin{})
 	gob.RegisterName("C", CommandCharacter{})
+	gob.RegisterName("A", CommandAnimation{})
+	gob.RegisterName("G", CommandGraphics{})
+	gob.RegisterName("O", CommandObject{})
+	gob.RegisterName("Oc", CommandObjectPayloadCreate{})
+	gob.RegisterName("Od", CommandObjectPayloadDelete{})
+	gob.RegisterName("Ot", CommandObjectPayloadTravel{})
+	gob.RegisterName("Om", CommandObjectPayloadMove{})
+	gob.RegisterName("Om", CommandObjectPayloadAnimate{})
 }
