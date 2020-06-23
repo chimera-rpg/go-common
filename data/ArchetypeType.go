9537decedd3b5e78483dab7e1f608a4e39dd8f3b
kts of kettek (yan) <kettek1@kettek.net>
2020-06-22 19:32:22 -0700

    Add UnmarshalYAML

--- data/ArchetypeType.go
+++ data/ArchetypeType.go
@@ -1,5 +1,7 @@
 package data
 
+import "fmt"
+
 // ArchetypeType is the numeric identifier for different archetype types.
 type ArchetypeType uint8
 
@@ -20,4 +22,38 @@ const (
 // AsUint8 returns ArchetypeType as a uint8.
 func (atype ArchetypeType) AsUint8() uint8 {
 	return uint8(atype)
+}
+
+// UnmarshalYAML unmarshals an ArchetypeType from a string.
+func (atype *ArchetypeType) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var value string
+	if err := unmarshal(&value); err != nil {
+		return err
+	}
+	switch value {
+	case "Genus":
+		*atype = ArchetypeGenus
+	case "Species":
+		*atype = ArchetypeSpecies
+	case "PC":
+		*atype = ArchetypePC
+	case "NPC":
+		*atype = ArchetypeNPC
+	case "Tile":
+		*atype = ArchetypeTile
+	case "Floor":
+		*atype = ArchetypeFloor
+	case "Wall":
+		*atype = ArchetypeWall
+	case "Item":
+		*atype = ArchetypeItem
+	case "Bullet":
+		*atype = ArchetypeBullet
+	case "Generic":
+		*atype = ArchetypeGeneric
+	default:
+		*atype = ArchetypeUnknown
+		return fmt.Errorf("Unknown Type '%s'", value)
+	}
+	return nil
 }
