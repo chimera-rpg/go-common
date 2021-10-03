e5de7a287b01012460166ed983dc790566f2e65a
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-10-02 23:13:15 -0700

    Add StatusMapToString and add comments

--- data/StatusType.go
+++ data/StatusType.go
@@ -3,12 +3,21 @@ package data
 // StatusType represents various Status effects.
 type StatusType uint8
 
-StringToStatusMap = map[string]StatusType{
-	"Falling": Falling,
+// StringToStatusMap is as the name implies.
+var StringToStatusMap = map[string]StatusType{
+	"Falling":   Falling,
 	"Squeezing": Squeezing,
 	"Crouching": Crouching,
 }
 
+// StatusMapToString is as the name implies.
+var StatusMapToString = map[StatusType]string{
+	Falling:   "Falling",
+	Squeezing: "Squeezing",
+	Crouching: "Crouching",
+}
+
+// Our various status types.
 const (
 	Falling StatusType = 0 << iota
 	Squeezing
