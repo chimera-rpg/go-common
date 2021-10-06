7ecdc489248b2391cd06906d61f6a088e82d22a3
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-10-06 01:13:34 -0700

    Add running status

--- data/StatusType.go
+++ data/StatusType.go
@@ -9,6 +9,7 @@ const (
 	FallingStatus StatusType = 1 << iota
 	SqueezingStatus
 	CrouchingStatus
+	RunningStatus
 )
 
 // StringToStatusMap is as the name implies.
@@ -16,6 +17,7 @@ var StringToStatusMap = map[string]StatusType{
 	"Falling":   FallingStatus,
 	"Squeezing": SqueezingStatus,
 	"Crouching": CrouchingStatus,
+	"Running":   RunningStatus,
 }
 
 // StatusMapToString is as the name implies.
@@ -23,4 +25,5 @@ var StatusMapToString = map[StatusType]string{
 	FallingStatus:   "Falling",
 	SqueezingStatus: "Squeezing",
 	CrouchingStatus: "Crouching",
+	RunningStatus:   "Running",
 }
